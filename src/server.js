90bf2d48db56c46a2f578604f77005b56da71a14
Robson Sousa <robson.sousa21@gmail.com>
2019-10-09 22:16:59 -0300

    Implemented comunication with websocket

--- src/server.js
+++ src/server.js
@@ -1,15 +1,37 @@
 const express = require("express");
 const mongoose = require("mongoose");
-const routes = require("./routes");
 const cors = require("cors");
 const path = require("path");
 
+const socketio = require("socket.io");
+const http = require("http");
+
+const routes = require("./routes");
+
 const app = express();
+const server = http.Server(app);
+const io = socketio(server);
 
 mongoose.connect("mongodb://localhost:27017/mongoaircnc", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
+
+const connectedUsers = {};
+
+io.on("connection", socket => {
+  const { user_id } = socket.handshake.query;
+
+  connectedUsers[user_id] = socket.id;
+});
+
+app.use((req, res, next) => {
+  req.io = io;
+  req.connectedUsers = connectedUsers;
+
+  return next();
+});
+
 // req.query = acessar query params (para filtro)
 // req.params = acessar acessar route params (para edição e delete)
 // req.body = acessar corpo da requisição (para ciração e edição)
@@ -19,4 +41,4 @@ app.use(express.json());
 app.use("/files", express.static(path.resolve(__dirname, "..", "uploads")));
 app.use(routes);
 
-app.listen(3330);
+server.listen(3330);
